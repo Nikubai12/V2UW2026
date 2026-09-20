e1a1a6091077a11e84114c1162861e6f0a325df1
Nikubai12 <68363007+Nikubai12@users.noreply.github.com>
2026-09-20 12:42:41 -0700

    Update data.js

--- data.js
+++ data.js
@@ -36,18 +36,20 @@ const PIE_CATEGORIES = [
       {
         name: "Joe N",
         org: "PCL",
-        raised: "150",
+        raised: "250",
         donors: [
           {name: "Nikhil V", amount: 100},
+           {name: "Paige", amount: 100},
            {name: "Steve Bigoray", amount: 50}
         ]
       },
        {
         name: "Ginger B",
         org: "PCL",
-        raised: "200",
+        raised: "300",
         donors: [
            {name: "Anonymous", amount: 200},
+           {name: "Nikhil V", amount: 100},
         ]
       },
       {
@@ -69,34 +71,38 @@ const PIE_CATEGORIES = [
       {
         name: "Pratthosh R",
         org: "PCL",
-        raised: "500",
+        raised: "600",
         donors: [
            {name: "Stefan Kendel", amount: 400},
+           {name: "Joe N", amount: 100},
            {name: "Steve Bigoray", amount: 50},
            {name: "Steven Webber", amount: 50},
+           
         ]
       },
       {
         name: "Nikhil V",
         org: "PCL",
-        raised: "650",
+        raised: "840",
         donors: [
            {name: "Adam Mastel", amount: 200},
-           {name: "Julie Watkins", amount: 100},
+           {name: "Julie Watkins", amount: 110},
            {name: "Brett Trepanier", amount: 100},
            {name: "Steve Bigoray", amount: 50},
            {name: "Dan Cettiga", amount: 50},
            {name: "Marty Zellweger", amount: 50},
            {name: "Stefan Kendel", amount: 50},
            {name: "Emil Ilagan", amount: 50},
+           {name: "Hot Dogs", amount: 120},
+           {name: "Graeme Deans", amount: 60},
         ]
       },
       {
         name: "Ramone B",
         org: "PCL",
-        raised: "1410",
+        raised: "1410.1",
         donors: [
-           {name: "Joseph and Buddies", amount: 1320},
+           {name: "Joseph and Buddies", amount: 1320.1},
            {name: "Graeme Deans", amount: 40},
            {name: "Steven Webber", amount: 50},
 
@@ -113,9 +119,11 @@ const PIE_CATEGORIES = [
 {
         name: "Brett T",
         org: "PCL",
-        raised: "50",
+        raised: "200",
         donors: [
-           {name: "Graeme Deans", amount: 50}
+           {name: "Jan", amount: 100},
+           {name: "Graeme Deans", amount: 50},
+           {name: "John Dyble", amount: 50},
         ]
       },
        
@@ -141,8 +149,9 @@ const PIE_CATEGORIES = [
        {
         name: "Blake C",
         org: "PCL",
-        raised: "660",
+        raised: "1010",
         donors: [
+           {name: "Stuart", amount: 350}
            {name: "Ramu", amount: 210},
            {name: "John K", amount: 100},
            {name: "Marty Zellweger", amount: 100},
@@ -150,6 +159,7 @@ const PIE_CATEGORIES = [
            {name: "Western Seaboard Transport", amount: 50},
            {name: "Anonymous", amount: 50},
            {name: "Anonymous", amount: 50}
+           
            
            
         ]
@@ -195,7 +205,11 @@ const PIE_CATEGORIES = [
    or "M:SS.mmm" style string (e.g. "1:12.4").
    ============================================================ */
 const HOTDOG_TIMES = [
-   { name: "Adam M",  org: "PCL", time: 30.0,   day: "Sept 20" },
+   { name: "Adam",  org: "PCL", time: 1:35.0,   day: "Sept 20" },
+   { name: "Derrick",  org: "PCL", time: 1:27.0,   day: "Sept 20" },
+   { name: "Jan",  org: "PCL", time: DNF,   day: "Sept 20" },
+   { name: "Paige",  org: "PCL", time: DNF,   day: "Sept 20" },
+   { name: "Caress",  org: "PCL", time: DNF,   day: "Sept 20" },
   // { name: "Jonothan G", org: "PCL", time: "1:04.9", day: "Sept 19" },
 ];
 
